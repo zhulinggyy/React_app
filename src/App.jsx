721c769b9723a8b4618ce7dcd6017ba01efc3d7d
zhulin <1455858964@qq.com>
2019-04-08 11:54:44 +0800

    '初始化仓库'

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import {Button} from 'antd';
+export default class App extends Component {
+  render () {
+    return (
+        <div>
+          <Button type='primary'>按键</Button>
+        </div>
+    )
+  }
+}
+
